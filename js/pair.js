d717a0d166061bc7e1dfbcf7353ed85aa9580f69
Joanne G. Basa <joannegbasa@gmail.com>
2017-09-21 21:00:13 +0100

    first working version

--- js/pair.js
+++ js/pair.js
@@ -5,10 +5,9 @@ $($ => {
 	let playerPairButton = $("#playerPairButton");
 	let pairedPlayers = $("#pairedPlayers");
 
-
 	// add player to list
 	playerForm.on("submit", (e) => {
-    e.preventDefault(); // prevent default form behaviour
+    e.preventDefault(); // prevents default form behaviour
 
     // get name from field
     let playerName = playerField.val();
@@ -19,56 +18,56 @@ $($ => {
     } else {
 			let playerNameItem = $('<li />', { text: playerName });
 
-	    // add to list
-	    playerList.append(playerNameItem);
-			}
 	    //clear field between adds
 	    playerField.val("");
+
+			// add to list
+	    playerList.append(playerNameItem);
+			}
 
   });
 
 	//pair players
 	playerPairButton.on("click", () => {
+		pairedPlayers.empty();
 		let people = [];
 		//pushing items into array and displaying items in console
 		$("#playerList li").each(function() { people.push($(this).text()) });
 
 		// this assumes an even number of players
 		if (people.length % 2 !== 0) {
-		   $("#morePlayers").append("You must have an even number of players. You currently have " + people.length + " to play.");
+			$("#morePlayers").empty();
+			$("#morePlayers").append("You must have an even number of players. You currently have " + people.length + " to play.");
 
 		} else {
-		  $("#morePlayers").remove(); //clears message once condition passes
 
-			//randomises players
-		  let splitNames = (i, people) => {
-		  let a = people.slice(0, i);
-		  let b = people.slice(i, people.length);
-		  return [a, b];
-			};
+			$("#morePlayers").empty(); //clears message once condition passes
 
 			let shuffle = people => {
-			  return people.slice(0).sort(() => {
-			    return 0.5 - Math.random();
-			  });
+				return people.slice(0).sort(() => {
+					return 0.5 - Math.random();
+				});
 			};
 
-			let paired = people => {
-			  return people[0].map((players, i) => {
-			    return people.map((players) => {
-			      return players[i];
-			    });
-			  });
+			let result = (people.length / 2, shuffle(people));
+
+			// pairs them in another array
+			let paired = [];
+
+     	for (var i=0; i<result.length; i=i+2) {
+    		paired.push(result.slice(i, i+2));
 			}
 
-	    let result = paired(splitNames(people.length / 2, shuffle(people)));
-
-			//split them into different lines
-			pairedPlayers.html(result.join("<br>"));
-
-			// add "vs" to every match ?map
+			// add "vs" to every match
+			for (var i=0; i<paired.length; i=i+1) {
+				let games = paired[i][0] + " vs " + paired[i][1];
+				let matches = $('<li />', { text: games });
+				pairedPlayers.append(matches);
+			}
 
 		}
+
 	});
+
 
 });
